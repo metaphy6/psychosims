c11cd80d6370961f8bc1edc7586c364c57bd8df6
serhatakbak <serhat.akbak@gmail.com>
2026-07-18 14:03:08 +0300

    refactor: flutter doctor

--- app/test/career_persistence_test.dart
+++ app/test/career_persistence_test.dart
@@ -54,7 +54,7 @@ void main() {
         ownedCases: {
           'case-1': CaseHistoryEnvelope(
             carryOverDeltas: [
-              StructuredDelta(
+              const StructuredDelta(
                 rulesetVersion: '0.5.0',
                 axis: StateAxis.trust,
                 deltaMillis: 5,
